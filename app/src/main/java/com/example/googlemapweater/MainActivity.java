7091229c7eb5f0ff7678870ec73fb560d3882254
KROSSok <omg.kross@gmail.com>
2020-04-27 20:44:57 +0300

    Initial

--- app/src/main/java/com/example/googlemapweater/MainActivity.java
+++ app/src/main/java/com/example/googlemapweater/MainActivity.java
@@ -7,19 +7,27 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+
+import android.widget.ImageView;
 import android.widget.SearchView;
+
 import android.widget.Toast;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 
+
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,11 +35,15 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -42,7 +54,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "MainActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -55,14 +67,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
 
+    private PlacesAutoCompleteAdapter mPlacesAutoCompleteAdapter;
+
+    private ImageView mGps;
+    private Marker mMarker;
+
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGps = findViewById(R.id.ic_gps);
+
         if(isServicesOK()){
             getLocationPermission();
         }
+
     }
+
 
     private void geoLocate(String searchString) {
         Log.d(TAG, "geoLocate: geolocating");
@@ -115,6 +144,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return;
             }
             mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
         }
     }
 
@@ -148,6 +178,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MarkerOptions options = new MarkerOptions().position(latLng).title("WEATHER INFO " + weatherInfo);
         mMap.addMarker(options);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        hideSoftKeyboard();
     }
 
     private void initMap(){
@@ -217,7 +248,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     moveCamera(new LatLng(lat, lon), DEFAULT_ZOOM, data.getCity() + " +" + data.getTemp() + " C " + data.getWeather());
                 }
             }
-
             @Override
             public void onFailure(Call<WeatherResponse> call, Throwable t) {
                 Log.e(TAG, "onFailure");
@@ -228,25 +258,51 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         MenuItem searchViewItem = menu.findItem(R.id.action_search);
         final SearchView searchView = (SearchView) searchViewItem.getActionView();
         searchView.setMaxWidth(Integer.MAX_VALUE);
         searchView.setQueryHint("search for city");
+
+        AutocompleteSessionToken autocompleteSessionToken;
+        autocompleteSessionToken = AutocompleteSessionToken.newInstance();
+        Places.initialize(getApplicationContext(), "AIzaSyBR0nYtZuIijQwbl2ZA5mVTrx1BPqvjaGg");
+
+        // Create a new Places client instance.
+        PlacesClient placesClient = Places.createClient(this);
+        mPlacesAutoCompleteAdapter = new PlacesAutoCompleteAdapter(this, placesClient,autocompleteSessionToken);
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 geoLocate(query);
+                hideSoftKeyboard();
                 return true;
             }
             @Override
             public boolean onQueryTextChange(String newText) {
+                mPlacesAutoCompleteAdapter.getFilter().filter(newText);
                 return false;
             }
         });
+
+        mGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked gps icon");
+                getDeviceLocation();
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
     }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
 }
 
 
